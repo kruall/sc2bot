5ce51f4659297e000f263ff14a524633f72557d3
Kryukov Alexander <kru.bash.all@gmail.com>
2020-08-22 20:08:50 +0300

    Fix include and redefinition in atomic_forward_list.h

--- src/core/actors/atomic_forward_list.h
+++ src/core/actors/atomic_forward_list.h
@@ -1,84 +1,12 @@
 #pragma once
 
-#include <value_holder_node.h>
+#include "value_holder_node.h"
 
 #include <atomic>
 #include <memory>
 
 
 namespace core::actors {
-
-    template <typename Derived>
-    struct ForwardListNode {
-        using self_type = ForwardListNode;
-        using derived_type = Derived;
-
-
-        ForwardListNode() = default;
-        ForwardListNode(const self_type&) = delete;
-        ForwardListNode(self_type&) = delete;
-
-        virtual ~ForwardListNode() = default;
-
-        std::unique_ptr<derived_type> Detach() {
-            return std::move(Next);
-        }
-
-        void Attach(std::unique_ptr<derived_type> &&node) {
-            Next = std::move(node);
-        }
-
-    protected:
-        std::unique_ptr<derived_type> Next = nullptr;
-    };
-
-    template <typename T>
-    struct ValueHolder {
-        using value_type = T;
-        using reference = T&;
-        using const_reference = const T&;
-
-
-        template <typename ...Args>
-        ValueHolder(Args ...args)
-            : Value(args...)
-        {}
-
-        reference operator->() {
-            return Value;
-        }
-
-        const_reference operator->() const {
-            return Value;
-        }
-
-        reference Get() {
-            return Value;
-        }
-
-        const_reference Get() const {
-            return Value;
-        } 
-
-    protected:
-        value_type Value;
-    };
-
-    template <typename T>
-    struct ValueHolderNode : ForwardListNode<ValueHolderNode<T>>, ValueHolder<T> {
-        using value_type = T;
-        using value_holder_type = ValueHolder<value_type>;
-        using self_type = ValueHolderNode<value_type>;
-        using base_node_type = ForwardListNode<self_type>;
-        using typename value_holder_type::reference;
-        using typename value_holder_type::const_reference;
-
-
-        template <typename ...Args>
-        ValueHolderNode(Args ...args)
-            : value_holder_type(args...)
-        {}
-    };
 
     template <typename Node, typename Interface>
     struct AtomicForwardListBase {
